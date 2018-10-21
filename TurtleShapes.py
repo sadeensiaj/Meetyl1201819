cc673dbba6b30207c389980345382ebaaee24a72
sadeen20-meet <sadeen20@meet.mit.edu>
2018-10-21 12:27:01 -0400

    lab3 is not done

---
+++ TurtleShapes.py
@@ -0,0 +1,10 @@
+import turtle
+angle=145
+length=120
+for i in range(5):
+	turtle.forward(length)
+	turtle.right(angle)
+
+
+
+turtle.mainloop()
--- lab2.py
+++ lab2.py
@@ -9,3 +9,14 @@ def less_than_five(d):
 		if number  < 5 :
 			print number
 less_than_five(s)
+l2=[1,20,30,40,50,60,70]
+l3=[1,10,20,40,60,4]
+l4=[]
+for x in l2:
+	for y in l3:
+		if x==y:
+			l4.append(x)
+print l4
+
+
+	
