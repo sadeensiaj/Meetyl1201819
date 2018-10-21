5025b94bb0bd79c243ff1a015a39b1c7855d6775
sadeen20-meet <sadeen20@meet.mit.edu>
2018-10-21 10:40:41 -0400

    first commit

---
+++ lab1.py
@@ -0,0 +1,19 @@
+print("sadeen")
+print("sadeen"*3)
+print("sadeen"*100)
+number1=4
+print(number1)
+number2=number1/2
+print(number2)
+list1=[2,4,6]
+for numbers in list1:
+	print numbers
+	print numbers*2
+print (list1[0]+list1[1]+list1[2])
+
+import turtle
+turtle.circle(100)
+turtle.goto(100,100)
+
+turtle.mainloop()
+
---
+++ lab2.py
@@ -0,0 +1,11 @@
+list1=[1,2,3,4]
+def make_list ():
+	a=[list1[0],list1[3]]
+	print(a)
+make_list()
+s=[1,2,4,5,8,9]
+def less_than_five(d):
+	for number in s:
+		if number  < 5 :
+			print number
+less_than_five(s)
